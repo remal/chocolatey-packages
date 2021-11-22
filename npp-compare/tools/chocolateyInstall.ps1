876fbc7e5cea766a48741203519a3011c77dbee5
Semyon Levin <levin.semen@gmail.com>
2021-11-22 17:18:20 +0300

    npp-compare

---
+++ npp-compare/tools/chocolateyInstall.ps1
@@ -0,0 +1,45 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+# @TODO: query HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Notepad++ to find installation path
+
+$registryInstallationPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Notepad++"
+$portableInstallationPath = "${env:AllUsersProfile}\chocolatey\lib\notepadplusplus.commandline\tools"
+if (Test-Path $registryInstallationPath){
+  $NotepadPlusPlusApplicationPath = Split-Path -Parent -Path (
+    Get-ItemProperty -Path $registryInstallationPath
+  ).DisplayIcon
+}
+elseif (Test-Path $portableInstallationPath){
+  $NotepadPlusPlusApplicationPath = $portableInstallationPath
+}
+
+$NotepadPlusPlusProcessName = 'Notepad++'
+$assetsToCopy = 'plugins'
+
+$packageArgs = @{
+  PackageName = 'npp-compare'
+  UnzipLocation = "${toolsPath}/plugins/ComparePlugin"
+  Url64bit = 'https://github.com/pnedev/compare-plugin/releases/download/v2.0.1/ComparePlugin_v2.0.1_X64.zip'
+  Checksum64 = '77DEDF98EA2280528D726C0053DB2001E90DA7588E14EE01A98933F121BB15CB'
+  ChecksumType64 = 'sha256'
+  Url = 'https://github.com/pnedev/compare-plugin/releases/download/v2.0.1/ComparePlugin_v2.0.1_x86.zip'
+  Checksum = '07972C1C7E3012A46AC6EF133A6500CA851BDDC9C83471DF2F118519A0241ED5'
+  ChecksumType = 'sha256'
+  SoftwareName = 'npp-compare*'
+}
+Install-ChocolateyZipPackage @packageArgs
+
+if ( Test-Path -Path $NotepadPlusPlusApplicationPath ){
+  Get-Process -Name $NotepadPlusPlusProcessName -ErrorAction SilentlyContinue | ForEach-Object {
+    Write-Warning "Closing running instances of Notepad++, please save your files"
+    $_.CloseMainWindow() | Out-Null
+  }
+  Start-Sleep -Milliseconds 500
+  if ($VerbosePreference){
+    Get-Process -Name $NotepadPlusPlusProcessName -ErrorAction SilentlyContinue
+  }
+  Copy-Item -Path $toolsPath/* -Include $assetsToCopy -Destination $NotepadPlusPlusApplicationPath -Force -Recurse -Confirm:$False -Verbose:$VerbosePreference
+} else {
+  throw "Cannot find installation of Notepad++"
+}
---
+++ npp-compare/tools/chocolateyUninstall.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+# @TODO: query HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Notepad++ to find installation path
+
+$registryInstallationPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Notepad++"
+$portableInstallationPath = "${env:AllUsersProfile}\chocolatey\lib\notepadplusplus.commandline\tools"
+if (Test-Path $registryInstallationPath){
+  $NotepadPlusPlusApplicationPath = Split-Path -Parent -Path (
+    Get-ItemProperty -Path $registryInstallationPath
+  ).DisplayIcon
+}
+elseif (Test-Path $portableInstallationPath){
+  $NotepadPlusPlusApplicationPath = $portableInstallationPath
+}
+
+$NotepadPlusPlusProcessName = 'Notepad++'
+
+if ( Test-Path -Path $NotepadPlusPlusApplicationPath ){
+  Get-Process -Name $NotepadPlusPlusProcessName -ErrorAction SilentlyContinue | ForEach-Object {
+    Write-Warning "Closing running instances of Notepad++, please save your files"
+    $_.CloseMainWindow() | Out-Null
+  }
+  Start-Sleep -Milliseconds 500
+  if ($VerbosePreference){
+    Get-Process -Name $NotepadPlusPlusProcessName -ErrorAction SilentlyContinue
+  }
+  Remove-Item -Path "${NotepadPlusPlusApplicationPath}/plugins/ComparePlugin" -Recurse -Force -Confirm:$False -Verbose:$VerbosePreference
+} else {
+  throw "Cannot find installation of Notepad++"
+}
