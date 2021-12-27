5a26850f2bebeef158b3640761a9e5e5b3bd9a82
Semyon Levin <levin.semen@gmail.com>
2021-12-27 21:04:43 +0400

    remal-hosts

---
+++ remal-hosts/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$toolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+foreach($line in Get-Content "$toolsPath/hosts-forbidden.txt") {
+  $line = $line.Trim()
+  if ($line -ne '') {
+    & hosts add 255.255.255.255 $line
+  }
+}
