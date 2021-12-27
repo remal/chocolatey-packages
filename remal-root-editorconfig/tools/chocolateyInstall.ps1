5fec76ec28d1fcdb1e0147b483978f19450b75f8
Semyon Levin <levin.semen@gmail.com>
2021-12-27 16:15:28 +0400

    remal-root-editorconfig

---
+++ remal-root-editorconfig/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$toolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$assetsPath = "$toolsPath/assets"
+
+if ("${env:HomeDrive}" -ne '') {
+  Copy-Item -Path "$assetsPath/*" -Destination "${env:HomeDrive}/" -Force -Recurse -Verbose:$VerbosePreference
+}
+
+if ("${env:SystemDrive}" -ne '') {
+  Copy-Item -Path "$assetsPath/*" -Destination "${env:SystemDrive}/" -Force -Recurse -Verbose:$VerbosePreference
+}
---
+++ remal-root-editorconfig/tools/chocolateyUninstall.ps1
@@ -0,0 +1,15 @@
+﻿$toolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$assetsPath = "$toolsPath/assets"
+[array]$files = Get-ChildItem $assetsPath -Recurse -Force | select -expand name
+
+foreach ($file in $files) {
+  if (("${env:HomeDrive}" -ne '') -and (Test-Path "${env:HomeDrive}/$file")) {
+    Write-Host "Removing ${env:HomeDrive}/$file"
+    Remove-Item -Path "${env:HomeDrive}/$file" -Force -Verbose:$VerbosePreference
+  }
+
+  if (("${env:SystemDrive}" -ne '') -and (Test-Path "${env:SystemDrive}/$file")) {
+    Write-Host "Removing ${env:SystemDrive}/$file"
+    Remove-Item -Path "${env:SystemDrive}/$file" -Force -Verbose:$VerbosePreference
+  }
+}
