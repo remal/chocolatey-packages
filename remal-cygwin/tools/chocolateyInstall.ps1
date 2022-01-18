985162451f9366af0cb448d3af6addac53e293d8
Semyon Levin <levin.semen@gmail.com>
2022-01-18 15:04:02 +0400

    remal-cygwin

---
+++ remal-cygwin/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$cygwinPath = (Get-ItemProperty 'HKLM:\SOFTWARE\Cygwin\setup' -ea 0).rootdir
+if (!$cygwinPath) {
+  $toolsPath = Get-ToolsLocatio
+  $cygwinPath = "$toolsPath\cygwin"
+}
+if (!(Test-Path $cygwinPath)) {
+  throw "Cannot find installation dir of Cygwin"
+}
+
+Install-ChocolateyEnvironmentVariable -VariableName 'CYGWIN_PATH' -VariableValue $cygwinPath -VariableType 'Machine'
+Install-RemalPath -PathToInstall "$cygwinPath\bin" -PathType 'Machine'
---
+++ remal-cygwin/tools/chocolateyUninstall.ps1
@@ -0,0 +1,4 @@
+﻿$cygwinPath = Get-EnvironmentVariable -Name 'CYGWIN_PATH' -Scope 'Machine'
+
+Uninstall-ChocolateyEnvironmentVariable -VariableName 'CYGWIN_PATH' -VariableType 'Machine'
+Uninstall-RemalPath -PathToUninstall "$cygwinPath\bin" -PathType 'Machine'
