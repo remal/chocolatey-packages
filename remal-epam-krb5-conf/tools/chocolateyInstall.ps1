38ec67b3cead046a478617a5a50088349e3f77a5
Semyon Levin <levin.semen@gmail.com>
2022-02-10 13:15:48 +0400

    remal-epam-krb5-conf

---
+++ remal-epam-krb5-conf/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$toolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$assetsPath = "$toolsPath/assets"
+
+if ("${env:WINDIR}" -ne '') {
+  Copy-Item -Path "$assetsPath/*" -Destination "${env:WINDIR}/" -Force -Recurse -Verbose:$VerbosePreference
+}
+
+if ("${env:SYSTEMROOT}" -ne '') {
+  Copy-Item -Path "$assetsPath/*" -Destination "${env:SYSTEMROOT}/" -Force -Recurse -Verbose:$VerbosePreference
+}
---
+++ remal-epam-krb5-conf/tools/chocolateyUninstall.ps1
@@ -0,0 +1,15 @@
+﻿$toolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$assetsPath = "$toolsPath/assets"
+[array]$files = Get-ChildItem $assetsPath -Recurse -Force | select -expand name
+
+foreach ($file in $files) {
+  if (("${env:WINDIR}" -ne '') -and (Test-Path "${env:WINDIR}/$file")) {
+    Write-Host "Removing ${env:WINDIR}/$file"
+    Remove-Item -Path "${env:WINDIR}/$file" -Force -Verbose:$VerbosePreference
+  }
+
+  if (("${env:SYSTEMROOT}" -ne '') -and (Test-Path "${env:SYSTEMROOT}/$file")) {
+    Write-Host "Removing ${env:SYSTEMROOT}/$file"
+    Remove-Item -Path "${env:SYSTEMROOT}/$file" -Force -Verbose:$VerbosePreference
+  }
+}
