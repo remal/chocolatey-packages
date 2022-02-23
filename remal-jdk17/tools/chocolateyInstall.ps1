d253055ae5c6019bffd45fbf0cd695998e214e1c
Semyon Levin <levin.semen@gmail.com>
2022-02-23 21:01:59 +0400

    remal-jdk17:17.0.2

--- remal-jdk17/tools/chocolateyInstall.ps1
+++ remal-jdk17/tools/chocolateyInstall.ps1
@@ -1,9 +1,7 @@
 ﻿$jdkArgs = @{
   version = '17'
-  url64bit = 'https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.1%2B12/OpenJDK17U-jdk_x64_windows_hotspot_17.0.1_12.msi'
-  checksum64bit = 'F5241BB43B589E45B6C3133FAE6CC8CBFFEC4CAE2504C3B99C3FAC0C50CBF11E'
-  url32bit = 'https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.1%2B12/OpenJDK17U-jdk_x86-32_windows_hotspot_17.0.1_12.msi'
-  checksum32bit = 'EEAA82A79AE7A488EFE152DF121F11AB9D2B9554DEB2AFE8B4A4110297205367'
+  url64bit = 'https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.2%2B8/OpenJDK17U-jdk_x64_windows_hotspot_17.0.2_8.msi'
+  checksum64bit = 'F66F21F0B25E4FE0449C41555DBEB7BA890D54E5A6E48E35B1A6309409EAF2D1'
 }
 
 Install-RemalAdoptiumJdk @jdkArgs
