3d30c17ffa55cfac224b562da039e1d42c9408a7
Semyon Levin <levin.semen@gmail.com>
2023-10-09 18:09:41 -0700

    fixes

--- remal-nodejs/tools/chocolateyInstall.ps1
+++ remal-nodejs/tools/chocolateyInstall.ps1
@@ -1,7 +1,4 @@
-﻿$ErrorActionPreference = "Stop"
-
-
-$identities = "BUILTIN\Users", "NT AUTHORITY\Authenticated Users"
+﻿$identities = "BUILTIN\Users", "NT AUTHORITY\Authenticated Users"
 foreach ($identity in $identities) {
     $acl = Get-Acl "${env:ProgramFiles}\nodejs"
     $acl.SetAccessRule((New-Object System.Security.AccessControl.FileSystemAccessRule($identity, "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")))
@@ -14,9 +11,11 @@ if ($? -eq $false) { throw "<Error Exit>" }
 
 
 $yarnVersion = '3.6.4' # renovate: datasource=npm depName=@yarnpkg/cli
-$targetFile = "${env:LOCALAPPDATA}\node\corepack\yarn\${yarnVersion}\yarn.js"
+
+$targetDir = "${env:LOCALAPPDATA}\node\corepack\yarn\${yarnVersion}"
+$targetFile = "${targetDir}\yarn.js"
 $url = "https://repo.yarnpkg.com/${yarnVersion}/packages/yarnpkg-cli/bin/yarn.js"
 Get-WebFile -FileName $targetFile -Url $url
 
-& corepack prepare "yarn@${yarnVersion}" --activate
+& corepack install --global "yarn@${yarnVersion}"
 if ($? -eq $false) { throw "<Error Exit>" }
