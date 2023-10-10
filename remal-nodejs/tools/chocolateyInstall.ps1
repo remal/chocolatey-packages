cdec7b8a5951808cc58eb0c5393d95869b52a66f
Semyon Levin <levin.semen@gmail.com>
2023-10-09 17:29:01 -0700

    fixes

--- remal-nodejs/tools/chocolateyInstall.ps1
+++ remal-nodejs/tools/chocolateyInstall.ps1
@@ -1,4 +1,7 @@
-﻿$identities = "BUILTIN\Users", "NT AUTHORITY\Authenticated Users"
+﻿$ErrorActionPreference = "Stop"
+
+
+$identities = "BUILTIN\Users", "NT AUTHORITY\Authenticated Users"
 foreach ($identity in $identities) {
     $acl = Get-Acl "${env:ProgramFiles}\nodejs"
     $acl.SetAccessRule((New-Object System.Security.AccessControl.FileSystemAccessRule($identity, "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")))
@@ -10,6 +13,14 @@ foreach ($identity in $identities) {
 
 
 $yarnVersion = '3.6.4' # renovate: datasource=npm depName=@yarnpkg/cli
-Get-WebFile -FileName "${env:LOCALAPPDATA}\node\corepack\yarn\${yarnVersion}\yarn.js" -Url "https://repo.yarnpkg.com/${yarnVersion}/packages/yarnpkg-cli/bin/yarn.js"
+$tergetFile = "${env:LOCALAPPDATA}\node\corepack\yarn\${yarnVersion}\yarn.js"
+$url = "https://repo.yarnpkg.com/${yarnVersion}/packages/yarnpkg-cli/bin/yarn.js"
+
+$targetDir = [System.IO.Path]::GetDirectoryName($tergetFile)
+if (!(Test-Path($targetDir))) {
+    [System.IO.Directory]::CreateDirectory($targetDir) | Out-Null
+}
+
+Get-WebFile -FileName $tergetFile -Url $url
 
 & corepack prepare "yarn@${yarnVersion}" --activate
