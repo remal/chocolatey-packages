dcadbbcfc834a3c0e8709e07ac4633def5b9f124
remal <remal@users.noreply.github.com>
2023-10-14 22:02:10 +0000

    [push-back] Push-back updated files during build

---
+++ remal-jdk21/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$jdkArgs = @{
+    version = '21'
+    url64bit = 'https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21%2B35/OpenJDK21U-jdk_x64_windows_hotspot_21_35.msi'
+    checksum64bit = '420b09998ae215154b6665c4d8167a74fd99eb3d4d85d5657ba317666e65e301'
+    url32bit = ''
+    checksum32bit = ''
+}
+Install-RemalAdoptiumJdk @jdkArgs
